d564ef1ddf8b7d5a93c29173a533de50dfef60f6
Rishabh Bhatnagar <rishabh.bhatnagar.rb@gmail.com>
2019-09-20 00:22:56 -0400

    Abstracted away hard coded gradle.

--- custom-ktlint-rules/src/main/java/com/therishib/custom_ktlint_rules/rules/NoForceUnwrapRule.kt
+++ custom-ktlint-rules/src/main/java/com/therishib/custom_ktlint_rules/rules/NoForceUnwrapRule.kt
@@ -5,12 +5,9 @@ import com.pinterest.ktlint.core.ast.isPartOfString
 import com.therishib.custom_ktlint_rules.extensions.isKtDoc
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.jetbrains.kotlin.lexer.KtTokens
 
-const val FORCE_UNWRAP_RULE_NAME = "no-force-unwraps"
-
-private const val FORCE_UNWRAP_TEXT: String = "!!"
-
-class NoForceUnwrapRule : Rule(FORCE_UNWRAP_RULE_NAME) {
+class NoForceUnwrapRule : Rule("no-force-unwraps") {
     override fun visit(
         node: ASTNode,
         autoCorrect: Boolean,
@@ -19,10 +16,15 @@ class NoForceUnwrapRule : Rule(FORCE_UNWRAP_RULE_NAME) {
         if (node is LeafPsiElement &&
             !node.isKtDoc() &&
             !node.isPartOfString() &&
-            !node.isPartOfString() &&
-            node.textMatches(FORCE_UNWRAP_TEXT)
+            node.textMatches(KtTokens.EXCLEXCL.value)
         ) {
-            emit(node.startOffset, "Force unwrapping at \"${node.text}\"", false)
+            emit(
+                node.startOffset,
+                "No Force Unwrapping (${KtTokens.EXCLEXCL.value}) allowed. " +
+                        "Please handle null case. " +
+                        "EG: With Elvis Operator (${KtTokens.ELVIS.value})",
+                false
+            )
         }
     }
 }
